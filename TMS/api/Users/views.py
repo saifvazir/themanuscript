bee74d1a65fa76a2e8840643dcd33182f66f0d75
Ankitesh Gupta <ankiteshguptas@gmail.com>
2016-12-04 15:36:17 +0530

    registration of user done

--- TMS/api/Users/views.py
+++ TMS/api/Users/views.py
@@ -5,8 +5,18 @@ from models import Users, tmsdb, Count
 import hashlib
 import datetime
 
-def checkusername(username):
-	Users.query.filter_by('Username'=Username).first()
+def checkusername(Username):
+	
+	try:
+		result = Users.query.filter_by(Username=Username).first()
+	except:
+		print "query not successfull"
+
+	if(result):
+		return True
+	else:
+		return False
+	
 
 def encrypt_password(password):
 	return hashlib.md5(password).hexdigest()
@@ -25,8 +35,37 @@ def generateuserid(username):
 def register():
 	data = json.loads(request.data)
 	print data
+	if(checkusername(data['Username'])):
+		return jsonify({
+			'payload':"failed"
+			'message':"username already exists"
+			})
+
+	User_id = generateuserid(data['Username'])
+	Username = data['Username']
+	Email_id = data['Email_id']
+	pwd = encrypt_password(data['Password'])
+	nw = datetime.datetime.now()
+	new_entry = Users(User_id,Username,Email_id,pwd,Dateentry=nw)
+	try:
+		tmsdb.session.add(new_entry)
+		tmsdb.session.commit()
+	except:
+		print "new user not entered some error occured"
+		return jsonify({
+			'payload':"failed"
+			'message':"some error occured please try again"
+			})
+
 	return jsonify({
-		
+			'payload':"Success"
+			'message':"User successfully created"
+			})
+
+
+
+	return jsonify({
+
 		})
 
 
